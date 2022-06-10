4f0ad949eb203bb5a1fbff8550a24630efa87a8d
mb <>
2022-06-10 17:05:25 +0200

    starting rust-by-example

--- 1firstProject/src/main.rs
+++ 1firstProject/src/main.rs
@@ -1,3 +1,19 @@
+use ferris_says::say; // from the previous step
+use std::io::{stdout, BufWriter};
+
 fn main() {
-    println!("Hello, world!");
+    let stdout = stdout();
+    let message = String::from("Hello fellow Rustaceans!");
+    let width = message.chars().count();
+
+    let mut writer = BufWriter::new(stdout.lock());
+    say(message.as_bytes(), width, &mut writer).unwrap();
+
+    let x = 5 + /* 90 + */ 5;
+    println!("Is `x` 10 or 100? x = {}", x);
+
+    let number: f64 = 1.0;
+    let offset: usize = 6;
+    println!("{number:>offset$}");
 }
+
