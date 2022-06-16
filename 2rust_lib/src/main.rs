5bdfb6acc5a443de620970bd3afb3b781d53de54
mb <>
2022-06-16 10:45:42 +0200

    libraries

--- 2rust_lib/src/main.rs
+++ 2rust_lib/src/main.rs
@@ -1,12 +1,13 @@
 // create:
-// rustc --crate-type=lib lib.rs
+// rustc --crate-type=lib mylib.rs
 
-
+// use:
+// rustc main.rs --extern mylib=libmylib.rlib --edition=2018 && ./main
 fn main() {
-    lib::public_function();
+    mylib::public_function();
 
     // Error! `private_function` is private
     //lib::private_function();
 
-    lib::indirect_access();
+    mylib::indirect_access();
 }
