de8dbfbd4118bea7edcc21b7e36a442bf24edcb8
mb <>
2022-06-24 12:50:55 +0200

    hello

--- 1firstProject/src/main.rs
+++ 1firstProject/src/main.rs
@@ -5,28 +5,12 @@ mod sections {
     pub use primitives::*;
     mod higher_order_fun;
     pub use higher_order_fun::*;
+    mod cfg;
+    pub use cfg::*;
 }
 pub use sections::*; // use all fn from sections
-use std::time::Instant;
 
 fn main() {
-    let upper = 100000000000;
-
-    // Imperative approach
-    let now = Instant::now();
-    let mut acc = 0_u64;
-    for n in 0.. {
-        let n_squared = n * n;
-
-        if n_squared >= upper {
-            break;
-        } else if is_odd(n_squared) {
-            acc += n_squared;
-        }
-    }
-    let elapsed_time = now.elapsed();
-
-    println!("imperative style took {:?} microseconds \n{}", elapsed_time.as_micros(), acc);
     // Primitives
     // begin()
     // debug()
@@ -34,7 +18,7 @@ fn main() {
     // primitive_types()
     // tuples()
     // arrays()
-    // higher_order()
-
+    higher_order()
+    // cfg()
  
 }
